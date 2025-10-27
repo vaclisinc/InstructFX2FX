64341d03198ab6f2f909d72e6aaaa61d13c4cee8
vaclis.mbp <nckusic2022@gmail.com>
2025-10-27 16:04:48 -0700

    Issue #10: Add audio loading utilities

--- baseline-system/judge_system/data/__init__.py
+++ baseline-system/judge_system/data/__init__.py
@@ -4,8 +4,14 @@ from judge_system.data.models import (
     SocialFXExample,
     DatasetMetadata,
 )
+from judge_system.data.audio_utils import (
+    load_audio_sample,
+    format_example_for_prompt,
+)
 
 __all__ = [
     "SocialFXExample",
     "DatasetMetadata",
+    "load_audio_sample",
+    "format_example_for_prompt",
 ]
