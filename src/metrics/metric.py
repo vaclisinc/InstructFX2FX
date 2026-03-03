cf4e631888fa4b54b35383f43bcd61db8ccd7238
Yuxuan Cai <yuxuancai1106@berkeley.edu>
2026-03-02 19:50:07 -0800

    metrics llm2fx

--- src/metrics/metric.py
+++ src/metrics/metric.py
@@ -1,19 +1,31 @@
 import torch
-from dataclasses import dataclass
 import abc
 from typing import Any
-from embeddings.clap import CLAPWrapper
-from prompts.prompt import Prompt
 
-class Metric (abc.ABC):
+
+class Metric(abc.ABC):
     @abc.abstractmethod
-    def compute(self, original_audio : Any, target_audio: Any, prompt : Prompt = None):
+    def compute(
+        self,
+        original_audio: Any,
+        target_audio: Any,
+        prompt: Any = None,
+    ) -> float:
         raise NotImplementedError("Must implement compute method in subclass")
 
-class CLAPSimilarity(Metric):
-    def __init__(self, device='cpu'):
-        super().__init__()
-        self.clap = CLAPWrapper(device='cuda' if torch.cuda.is_available() else device)
 
-    def compute(self, original_audio, target_audio, prompt : Prompt = None):
+class CLAPSimilarity(Metric):
+    def __init__(self, device="cpu"):
+        super().__init__()
+        from embeddings.clap import CLAPWrapper
+        self.clap = CLAPWrapper(
+            device="cuda" if torch.cuda.is_available() else device
+        )
+
+    def compute(
+        self,
+        original_audio: Any,
+        target_audio: Any,
+        prompt: Any = None,
+    ) -> float:
         raise NotImplementedError("CLAPSimilarity metric")
